a4781f54d159eaebb832f64b81556051543be5ad
Ryan Purpura <10505811+superiorbocal@users.noreply.github.com>
2018-07-29 16:51:38 -0700

    Updated CLI, adding more functionality and -h help strings

--- cli.py
+++ cli.py
@@ -28,8 +28,6 @@ def start(args):
     requested_category = args.category
     handler = set_up()
 
-    category = None
-
     if handler.is_active_session():
         print('An active session is running!')
         return
@@ -63,7 +61,7 @@ def start(args):
     handler.start(category)
 
 
-def end(args):
+def end(_):
     handler = set_up()
     if handler.is_active_session():
         handler.complete()
@@ -71,33 +69,68 @@ def end(args):
         print("There's no session running!")
 
 
-def status(args):
+def cancel(_):
+    handler = set_up()
+    if handler.is_active_session():
+        handler.cancel()
+    else:
+        print('Cannot cancel: No active session.')
+
+
+def status(_):
     handler = set_up()
     table, headers = table_generator.create_table_iterable_and_headers(
         data_handler=handler)
     print(tabulate(table, headers=headers))
+    print()
+    print('Active categories: {}'.format(', '.join(handler.active_categories)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     subparsers = parser.add_subparsers()
-    create_parser = subparsers.add_parser('create')
-    create_parser.add_argument('categories', metavar='C', nargs='+')
+    create_parser = subparsers.add_parser('create',
+                                          help='Create new categories')
+    create_parser.add_argument('categories', metavar='C', nargs='+',
+                               help='Categories to create. Active categories'
+                                    'that already exist will be ignored.')
     create_parser.set_defaults(func=create_categories)
 
-    start_subparser = subparsers.add_parser('start')
-    start_subparser.add_argument('category', metavar='C')
-    start_subparser.add_argument('-c', '--create', action='store_true')
+    start_subparser = subparsers.add_parser('start',
+                                            help='Start a time-tracking'
+                                                 'session with the given'
+                                                 'category.')
+    start_subparser.add_argument('category', metavar='C',
+                                 help='Category under which timer is started. '
+                                      'Abbreviated category name is supported '
+                                      'as long as unambiugous (e.g. "w" for '
+                                      'work')
+    start_subparser.add_argument('-c', '--create', action='store_true',
+                                 help='If flag is set, create the category '
+                                      'if it does not already exist. If the '
+                                      'flag is not set (default), then '
+                                      'nothing will happen.')
     start_subparser.set_defaults(func=start)
 
-    status_subparser = subparsers.add_parser('status')
+    status_subparser = subparsers.add_parser('status',
+                                             help='Displays the progress of '
+                                                  'the previous 7 days and '
+                                                  'displays the active '
+                                                  'categories.')
     status_subparser.set_defaults(func=status)
 
-    end_subparser = subparsers.add_parser('end')
+    end_subparser = subparsers.add_parser('end', help='Completes the current '
+                                                      'time-tracking session.')
     end_subparser.set_defaults(func=end)
 
+    cancel_subparser = subparsers.add_parser('cancel',
+                                             help='Cancels the current time-'
+                                                  'tracking session.')
+    cancel_subparser.set_defaults(func=cancel)
+
     parser.add_argument('-i', '--interactive', action='store_true')
+    # TODO: implement interactive
 
     args = parser.parse_args()
     args.func(args)
